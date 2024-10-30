ea2ad5b6c73a40268d059bbe21c7e29c2b6e8803
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-10-30 16:59:20 +0900

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by apple on 2024/10/30.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
